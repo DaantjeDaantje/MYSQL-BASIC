65e96dc30417e21ddf32d32f2adffa5f406c5a8b
DaantjeDaantje <danielleroelofsma@gmail.com>
2020-06-02 22:38:20 +0200

    taak 1

--- 1-databases/taak01/antwoorden.sql
+++ 1-databases/taak01/antwoorden.sql
@@ -1,1 +1,28 @@
 -- Taak01 - haal de tabel movies op:
+
+movie_id   	movie	rating	year	
+1	Velvet Buzzsaw	6	2019	
+2	Alita: Battle Angel	8	2019	
+3	Avengers: Endgame	0	2019	
+4	Polar	6	2019	
+5	Bohemian Rhapsody	8	2018	
+6	Overlord	7	2018	
+7	Glass	7	2019	
+8	Green Book	8	2018	
+9	Aquaman	7	2018	
+10	Extremely Wicked, Shockingly Evil, and Vile	8	2019	
+11	Child's Play	0	2019	
+12	The Favourite	8	2018	
+13	Captain Marvel	0	2019	
+14	A Star Is Born	8	2018	
+15	Incredibles 2	8	2018	
+16	Fast & Furious Presents: Hobbs & Shaw	0	2019	
+17	The Lego Movie 2: The Second Part	7	2019	
+18	Joker	0	2019	
+19	Mortal Engines	6	2018	
+20	Cold Pursuit	7	2019	
+21	How to Train Your Dragon: The Hidden World	8	2019	
+22	Once Upon a Time in Hollywood	0	2019	
+23	Widows	7	2018	
+24	Robin Hood	5	2018	
+25	Roma	8	2018	
